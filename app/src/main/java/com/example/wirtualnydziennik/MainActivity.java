fcba124e3d347fa46f43096f528fe58052b8afc6
Krzysiek <krzysiekksi6@gmail.com>
2022-03-15 13:55:25 +0100

    first commit

---
+++ app/src/main/java/com/example/wirtualnydziennik/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.wirtualnydziennik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int TIME_OUT_SPLASH = 4000;
+    Handler handler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+    }
+}
