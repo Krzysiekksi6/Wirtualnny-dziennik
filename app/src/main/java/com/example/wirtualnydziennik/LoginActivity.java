98f674036558d413c026567d93344a3c3c1e1ba1
Krzysiek <krzysiekksi6@gmail.com>
2022-03-16 18:34:31 +0100

    login and connect with fire database

--- app/src/main/java/com/example/wirtualnydziennik/LoginActivity.java
+++ app/src/main/java/com/example/wirtualnydziennik/LoginActivity.java
@@ -9,6 +9,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class LoginActivity extends AppCompatActivity {
     private TextView titleTextView, registerTextView, forgotPasswordTextView;
     private ImageView logoImageView;
@@ -26,6 +29,11 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.login_button);
         userEditText = findViewById(R.id.user_edit_text);
         passwordEditText = findViewById(R.id.password_edit_text);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
